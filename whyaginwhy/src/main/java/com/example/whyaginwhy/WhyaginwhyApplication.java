e410f555c3ee533941dae701325944473fec9935
Hajin <22100228@handong.ac.kr>
2024-11-16 01:57:52 +0900

    update

---
+++ whyaginwhy/src/main/java/com/example/whyaginwhy/WhyaginwhyApplication.java
@@ -0,0 +1,13 @@
+package com.example.whyaginwhy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyaginwhyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhyaginwhyApplication.class, args);
+    }
+
+}
---
+++ whyaginwhy/src/test/java/com/example/whyaginwhy/WhyaginwhyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whyaginwhy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyaginwhyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
